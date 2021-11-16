c9afa78edc87e13196ec8661b2ad7e147a3e490c
Mark Atherton <mark@insightinternet.co.uk>
2021-11-16 12:04:36 +0000

    set input value prop to {enteredName} so that the setEnterName fnction is called with an empty string to reset entered name value is removed on submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -11,8 +11,11 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     console.log(enteredName);
+
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
+
+    setEnteredName("");
   };
 
   return (
@@ -24,6 +27,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          value={enteredName}
         />
       </div>
       <div className="form-actions">
